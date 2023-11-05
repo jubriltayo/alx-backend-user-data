3abf4a0cca73bf34b91b072ceb45b6f6be1f33b6
Tayo Jubril <jubriltayo@gmail.com>
2023-11-05 23:31:23 +0100

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""This module encrypts personal data"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """obscure/redact return message"""
+    for word in fields:
+        message = re.sub(f"{word}=.*{separator}",
+                         f"{word}={redaction}{separator}",
+                         message
+                         )
+    return message
