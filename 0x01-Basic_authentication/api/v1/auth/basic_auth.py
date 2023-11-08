740bfa7d4c9779b63f98c57b7212e6ecb0569da8
Tayo Jubril <jubriltayo@gmail.com>
2023-11-08 18:06:43 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -23,13 +23,13 @@ class BasicAuth(Auth):
                 self,
                 base64_authorization_header: str,
                 ) -> str:
-            """ Decodes a base64-encoded authorization header """
-            if type(base64_authorization_header) == str:
-                try:
-                    res = base64.b64decode(
-                        base64_authorization_header,
-                        validate=True,
-                    )
-                    return res.decode('utf-8')
-                except (binascii.Error, UnicodeDecodeError):
-                    return None
+        """ Decodes a base64-encoded authorization header """
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
