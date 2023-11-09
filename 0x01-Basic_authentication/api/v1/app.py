bd9ec865bea2f36a7f7e21daeef6ab2bc01f17f1
Tayo Jubril <jubriltayo@gmail.com>
2023-11-09 10:57:38 +0100

    bug fixed

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
@@ -14,12 +13,12 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-auth_type = os.getenv("AUTH_TYPE")
+AUTH_TYPE = getenv("AUTH_TYPE")
 
-if auth_type == "auth":
+if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-if auth_type == "basic_auth":
+if AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
@@ -29,14 +28,14 @@ def before_request():
     """filter the user request"""
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401, description="Unauthorized")
-    if auth.current_user(request) is None:
-        abort(403, description="Forbidden")
+    else:
+        excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(404)
@@ -54,7 +53,7 @@ def unauthorized(error) -> str:
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
+def Forbidden(error) -> str:
     """ forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python3
-"""This module handles the API authentication"""
+"""Class Auth to manage the API authentication"""
 
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth():
-    """
-    Class for managing API authentication
-    """
+    """Auth class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if authentication is required for a given path"""
-        if excluded_paths is None or excluded_paths == [] or path is None:
+        """require_auth method"""
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
-        else:
-            if not path.endswith('/'):
-                path = path + '/'
-            if path in excluded_paths:
+
+        if len(path) == 14:
+            path = f"{path}/"
+
+        if path in excluded_paths:
+            return False
+
+        for ex_path in excluded_paths:
+            if ex_path.startswith(path):
                 return False
+            elif path.startswith(ex_path):
+                return False
+            if ex_path[-1] == "*":
+                if path.startswith(ex_path[:-1]):
+                    return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
-        """Extracts the authorization header from the request"""
-        return None
+        """authorization_header method"""
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Returns current user based on request"""
+        """current_user"""
         return None
