75d858e3fc9d10b0c58ee2dd99baac065ae9f495
Tayo Jubril <jubriltayo@gmail.com>
2023-11-09 06:32:09 +0100

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ from .auth import Auth
 import re
 import base64
 import binascii
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -33,3 +34,23 @@ class BasicAuth(Auth):
                 return res.decode('utf-8')
             except (binascii.Error, UnicodeDecodeError):
                 return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+            ) -> Tuple[str, str]:
+        """
+            Retrieves user credentials from the base64-decoded
+            authorization header
+        """
+        if type(decoded_base64_authorization_header) == str:
+            pattern = r'(?P<user>[^:]+):(?P<password>.+)'
+            field_match = re.fullmatch(
+                pattern,
+                decoded_base64_authorization_header.strip(),
+            )
+            if field_match is not None:
+                user = field_match.group('user')
+                password = field_match.group('password')
+                return user, password
+        return None, None
