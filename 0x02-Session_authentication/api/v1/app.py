7a70ea9060ca2287496bef8bd7b9fb970a23c296
Tayo Jubril <jubriltayo@gmail.com>
2023-11-11 08:07:32 +0100

    retrieve cookie

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,9 +35,8 @@ def before_request():
         excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
                           '/api/v1/forbidden/', '/api/v1/auth_session/login/']
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
-                abort(401)
-            if auth.session_cookie(request) is None:
+            cookie = auth.session_cookie(request)
+            if not auth.authorization_header(request) and not cookie:
                 abort(401)
             request.current_user = auth.current_user(request)
             if auth.current_user(request) is None:
