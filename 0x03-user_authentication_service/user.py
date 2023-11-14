0edd75feeface3e6ae08dc92903502b53304b6e8
Tayo Jubril <jubriltayo@gmail.com>
2023-11-14 12:56:33 +0100

    create table using sqlalchemy

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""This module defines an SQLAlchemy model named User"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Class User represents a user columns for mySQL table
+
+    __tablename__: name of table
+    id: user id
+    email: user email
+    hashed_password: user's encrypted/hashed password
+    seesion_id: id of session
+    reset_token: reset token
+    """
+
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    seesion_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
