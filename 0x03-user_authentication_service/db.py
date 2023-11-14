371848ef02970cd008c7469b74e1bc8f7aa976a2
Tayo Jubril <jubriltayo@gmail.com>
2023-11-14 16:45:42 +0100

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -55,7 +55,10 @@ class DB:
         try:
             user = self.find_user_by(id=user_id)
             for key, value in kwargs.items():
-                user.key = value
+                if hasattr(User, key):
+                    setattr(user. key, value)
+                else:
+                    raise ValueError
         except AttributeError:
             raise ValueError
         self._session.commit()
