9acee568727acf95fb59df82f922c3c6cea400d4
Tayo Jubril <jubriltayo@gmail.com>
2023-11-14 15:49:50 +0100

    search user in database by any of class attributes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,7 +40,7 @@ class DB:
         self._session.commit()
         return new_user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """finds user in database using kwargs"""
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
