06b2ea0aeb2f840da9109df7468e3ffbf7d04798
Tayo Jubril <jubriltayo@gmail.com>
2023-11-15 10:25:37 +0100

    register user via form

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 """This module defines a basic flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def home():
     """home page"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """register user"""
+    # retrieve data from users via form
+    data = request.form.to_dict()
+    try:
+        email = data.get('email')
+        password = data.get('password')
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": user.email, "message": "user created"})
 
 
 if __name__ == "__main__":
