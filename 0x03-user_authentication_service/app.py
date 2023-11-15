47f76214331a7a078672387e5d6ca4d52648cea8
Tayo Jubril <jubriltayo@gmail.com>
2023-11-15 17:36:41 +0100

    route for reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -83,8 +83,24 @@ def get_reset_password_token():
     try:
         user = AUTH._db.find_user_by(email=email)
         token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": user.email, "reset_token": token})
+        return jsonify({"email": user.email, "reset_token": token}), 200
     except Exception:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """update password"""
+    data = request.form.to_dict()
+    email = data.get('email')
+    reset_token = data.get('reset_token')
+    new_password = data.get('new_password')
+    try:
+        user = AUTH._db.find_user_by(email=email)
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": user.email,
+                        "message": "Password updated"}), 200
+    except ValueError:
         abort(403)
 
 
